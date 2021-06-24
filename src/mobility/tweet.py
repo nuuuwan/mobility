ba37bec9b264093a2632e27476f7a2d2c37698e6
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-24 16:44:37 +0530

    Refactored to use utils.twitter

--- src/mobility/tweet.py
+++ src/mobility/tweet.py
@@ -1,20 +1,11 @@
 """Tweet."""
 
-import argparse
-import tweepy
+from utils import twitter
 
-from utils import timex
-
-from mobility._utils import log
 from mobility.plot import _plot_all
 
 
-def _tweet(
-    twtr_api_key,
-    twtr_api_secret_key,
-    twtr_access_token,
-    twtr_access_token_secret,
-):
+def _tweet():
     plot_info = _plot_all()
 
     tweet_text = '''
@@ -27,58 +18,15 @@ Mobility Report {_ds}
         _ds=plot_info['ds'],
     )
 
-    log.info('Tweeting: %s', tweet_text)
-    log.info('Tweet Length: %d', len(tweet_text))
+    status_image_files = plot_info['image_files']
 
-    image_files = plot_info['image_files']
-    log.info('Status images: %s', ';'.join(image_files))
-
-    if not twtr_api_key:
-        return
-
-    auth = tweepy.OAuthHandler(twtr_api_key, twtr_api_secret_key)
-    auth.set_access_token(twtr_access_token, twtr_access_token_secret)
-    api = tweepy.API(auth)
-
-    media_ids = []
-    for image_file in image_files:
-        res = api.media_upload(image_file)
-        media_id = res.media_id
-        media_ids.append(media_id)
-        log.info('Uploaded image %s to twitter as %s', image_file, media_id)
-
-    log.info(api.update_status(tweet_text, media_ids=media_ids))
-
-    date = timex.format_time(timex.get_unixtime(), '%B %d, %Y %H:%M%p')
-    timezone = timex.get_timezone()
-    log.info(api.update_profile(
-        description='''Statistics about Sri Lanka.
-
-Automatically updated at {date} {timezone}
-        '''.format(date=date, timezone=timezone)
-    ))
+    twtr = twitter.Twitter.from_args()
+    twtr.tweet(
+        tweet_text=tweet_text,
+        status_image_files=status_image_files,
+        update_user_profile=True,
+    )
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Run pipeline for custom_dgigovlk_covid19.',
-    )
-    for twtr_arg_name in [
-        'twtr_api_key',
-        'twtr_api_secret_key',
-        'twtr_access_token',
-        'twtr_access_token_secret',
-    ]:
-        parser.add_argument(
-            '--' + twtr_arg_name,
-            type=str,
-            required=False,
-            default=None,
-        )
-    args = parser.parse_args()
-    _tweet(
-        args.twtr_api_key,
-        args.twtr_api_secret_key,
-        args.twtr_access_token,
-        args.twtr_access_token_secret,
-    )
+    _tweet()
