89e630a1df4a15368cb865d4a98d66b140e33727
Nuwan I. Senaratna <nuuuwan@gmail.com>
2021-06-27 13:19:27 +0530

    Update tweet.py

--- src/mobility/tweet.py
+++ src/mobility/tweet.py
@@ -20,13 +20,13 @@ def _tweet():
     latest_data_dsd = sorted(latest_data_dsd, key=lambda x: x[1])
     dsd_index = ents.get_entity_index('dsd')
 
-    rendered_detail_lines = ['🔴 HIGHEST']
+    rendered_detail_lines = ['🔴 MOST']
     for i in range(0, 3):
         info = latest_data_dsd[-(i + 1)]
         dsd = dsd_index[info[0]]
         name = dsd['name']
-        if len(name) > 10:
-            name = name.split(' ')[0][:10]
+        if len(name) > 15:
+            name = name.split(' ')[0][:15]
         name = '#' + name
         rendered_detail_lines.append(
             '{dsd_id} {p_non_mobile:.1%}'.format(
@@ -34,13 +34,13 @@ def _tweet():
                 p_non_mobile=info[1],
             )
         )
-    rendered_detail_lines += ['', '🟢 LOWEST']
+    rendered_detail_lines += ['', '🟢 LEAST']
     for i in range(0, 3):
         info = latest_data_dsd[i]
         dsd = dsd_index[info[0]]
         name = dsd['name']
-        if len(name) > 10:
-            name = name.split(' ')[0][:10]
+        if len(name) > 15:
+            name = name.split(' ')[0][:15]
         name = '#' + name
         rendered_detail_lines.append(
             '{dsd_id} {p_non_mobile:.1%}'.format(
@@ -51,12 +51,12 @@ def _tweet():
     rendered_details = '\n'.join(rendered_detail_lines)
 
     tweet_text = '''#Mobility Report ({_ds})
-% of Sri Lankans "Staying Put"
+% of #SriLanka "Staying Put"
 
 {rendered_details}
 
 Method: @Facebook Mobility
-#lka #SriLanka #COVID19SL #LockDown @HumanData
+#lka #COVID19SL @HumanData
     '''.format(
         _ds=latest_ds,
         rendered_details=rendered_details,
